cbb28e7cde06364efde0b07a4703ebd7d6ced697
Dustin N <newmandustin@sbcglobal.net>
2023-11-07 13:09:12 -0500

    fixed small error

--- payment_choice/models/__init__.py
+++ payment_choice/models/__init__.py
@@ -1,6 +1,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import payment_move
+from . import account_move
 from . import payment_provider
 from . import payment_token
 from . import payment_transaction
