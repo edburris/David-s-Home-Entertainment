ecbfe3ae66d75910645133627ca2bb6369c91c89
Dustin N <newmandustin@sbcglobal.net>
2023-11-16 17:12:54 -0500

    added ach auto run transaction system

--- payment_choice/models/payment_transaction.py
+++ payment_choice/models/payment_transaction.py
@@ -351,7 +351,7 @@ class PaymentTransaction(models.Model):
             response = res.json()
             _logger.info("********SEND PAYMENT REQUEST...: %s", response)
             if response['status'] == "Transaction - Approved": 
-                tx = self.search([('reference', '=', response['CustomerData']), ('provider_code', '=', 'choice')])
+                tx = self.search([('reference', '=', response['customData']), ('provider_code', '=', 'choice')])
                 tx.write({'provider_reference': response['guid']})
                 self._handle_notification_data('choice', response);
             else: 
