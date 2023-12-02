23cfc50c53a1f4a2ebf4fd47037ef888f737abb0
Fosterdn98 <newmandustin@sbcglobal.net>
2023-12-02 10:55:54 -0500

    attempt to get original invoice details

--- payment_choice/models/payment_transaction.py
+++ payment_choice/models/payment_transaction.py
@@ -81,7 +81,7 @@ class PaymentTransaction(models.Model):
             return res
         _logger.info("Choice _get_specific_rendering_values")
 
-        testReturn = self.env['account.move'].sudo().search(['name', 'ilike', 'self.reference'],limit=1)
+        testReturn = self.env['account.move'].sudo().search(domain=['name', 'ilike', self.reference],limit=1)
         _logger.info("TEST RETURN %s", testReturn)
 
         base_url = self.provider_id.get_base_url()
